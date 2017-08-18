65682dc7e2007769cb78ba0183e87ef9353ada21
aiyouheiha <aiyouheiha@163.com>
2017-08-18 15:57:24 +0800

    test

--- manager-web/src/main/resources/static/js/scripts.js
+++ manager-web/src/main/resources/static/js/scripts.js
@@ -1,25 +1,25 @@
-
 jQuery(document).ready(function() {
-
     $('.page-container form').submit(function(){
         var username = $(this).find('.username').val();
         var password = $(this).find('.password').val();
         if(username == '') {
+            var usernameObj =  $(this).parent().find('.username');
             $(this).find('.error').fadeOut('fast', function(){
-                $(this).css('top', '27px');
+                $(this).css('top', usernameObj.position().top);
             });
             $(this).find('.error').fadeIn('fast', function(){
-                $(this).parent().find('.username').focus();
+                usernameObj.focus();
             });
             return false;
         }
-		
+
         if(password == '') {
+            var passwordObj =  $(this).parent().find('.password');
             $(this).find('.error').fadeOut('fast', function(){
-                $(this).css('top', '96px');
+                $(this).css('top', passwordObj.position().top);
             });
             $(this).find('.error').fadeIn('fast', function(){
-                $(this).parent().find('.password').focus();
+                passwordObj.focus();
             });
             return false;
         }
@@ -28,5 +28,4 @@ jQuery(document).ready(function() {
     $('.page-container form .username, .page-container form .password').keyup(function(){
         $(this).parent().find('.error').fadeOut('fast');
     });
-
 });
